2737d34d0734995e2bd8a158c73cc61b5c36c914
Rizki Januar I <104696196+rzkjanuarr@users.noreply.github.com>
2023-05-24 21:18:45 +0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(
+  cors({
+    origin: [process.env.PUBLIC_URL],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is Running at http://localhost:${port}`);
+});
